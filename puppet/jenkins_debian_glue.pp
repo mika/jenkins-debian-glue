36cca6030a35af27b85bc885d99efd8039a5db33
Michael Prokop <prokop@grml-solutions.com>
2014-03-27 11:43:56 +0100

    Support skipping adt tests by setting ADT=skip
    Example to skip the adt tests for some packages and run them in a separate job instead:
    http://git.postgresql.org/gitweb/?p=pgapt.git;a=commitdiff;h=34ebf5a322d2c27b5d887fb871d16e7ace68dee
    
    NOTE: If you set ADT variable in your environment make sure to also
    allow ADT in sudo's env_keep configuration.
    
    Thanks: Christoph Berg

--- puppet/jenkins_debian_glue.pp
+++ puppet/jenkins_debian_glue.pp
@@ -216,7 +216,7 @@ class jenkins::software {
 
 # Make sure DEB_* options reach cowbuilder, like e.g.:
 #  export DEB_BUILD_OPTIONS="parallel=8" /usr/bin/build-and-provide-package
-Defaults  env_keep+="DEB_* DIST ARCH"
+Defaults  env_keep+="DEB_* DIST ARCH ADT"
 
 # for *-binaries job
 jenkins ALL=NOPASSWD: /usr/sbin/cowbuilder, /usr/sbin/chroot
